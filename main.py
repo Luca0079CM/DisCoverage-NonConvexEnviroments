Fix rotation

main.py
@@ -1,5 +1,8 @@
 import pygame as pg
 import math
+import numpy as np
+from random import *
+import time
 # Colors
 GRIDSIZE = 20
 BLACK = (0, 0, 0)
@@ -8,33 +11,37 @@ WHITE = (255, 255, 255)
 GREY = (160, 160, 160)
 LIGHTGREY = (190, 190, 190)
 GREEN = (0, 153, 76)
+RED = (255, 0, 0)
+BLUE = (0, 128, 255)
 
-#Window
+# Window
 pg.display.init()
 window = pg.display.set_mode((800, 600))
 robot_image = pg.image.load("robot1.png")
-circle_image = pg.image.load("circle.png").convert_alpha()
-pg.transform.scale(circle_image, (1, 1))
-#Speed
-v = 0.5
+# Speed
+v = 0.7
+# Delta
+pi = math.pi
+standard_delta = [0, pi/4, pi/2, pi*3/4, pi, pi*5/4, pi*3/2, pi*7/4]
 
 
 class Robot(pg.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
         self.image = robot_image
-        self.rect = self.image.get_rect(center=(x, y))
         self.x = x
         self.y = y
-        self.delta = 0
-        self.radius = 50
+        self.radius = 35
+        self.rect = self.image.get_rect(center=(x, y))
+        self.delta = standard_delta[1]
+        self.reachable_matrix = np.zeros([int(window.get_width() / GRIDSIZE), int(window.get_height() / GRIDSIZE)])
 
-    def update(self):
-        mx, my = pg.mouse.get_pos()
-        self.delta = math.atan2(self.x - mx, self.y - my)
-        self.x += v * math.cos(self.delta)
-        self.y += v * - math.sin(self.delta)
-        self.rect.x, self.rect.y = self.x, self.y
+    def update(self, matrix):
+        dx = -math.cos(self.delta - (math.pi / 2)) * v
+        dy = math.sin(self.delta - (math.pi / 2)) * v
+        self.x += dx
+        self.y += dy
+        self.rect = self.image.get_rect(center=(self.x, self.y))
 
 
 class MapRect(pg.sprite.Sprite):
@@ -42,7 +49,7 @@ class MapRect(pg.sprite.Sprite):
         super().__init__()
         self.rect = pg.Rect((x, y), (GRIDSIZE, GRIDSIZE))
         self.color = color
-        self.found = False
+        self.found = 0
         self.is_obstacle = False
 
 
@@ -52,20 +59,23 @@ def map_create():
     map = []
     for i in range(0, window.get_width(), GRIDSIZE):
         for j in range(0, window.get_height(), GRIDSIZE):
-            r = MapRect(i, j, GREY)
+            if ((i + j)/GRIDSIZE) % 2 == 0:
+                r = MapRect(i, j, GREY)
+            else:
+                r = MapRect(i, j, LIGHTGREY)
             if i == 0 or i == window.get_width() - GRIDSIZE or j == 0 or j == window.get_height() - GRIDSIZE:
                 r.is_obstacle = True
             pg.draw.rect(surface, r.color, r.rect)
             map.append(r)
 
-    #Ostacolo 1
+    # Ostacolo 1
     i = GRIDSIZE * 20
     for j in range(400, window.get_height(), GRIDSIZE):
         for m in map:
             if m.rect.x == i and m.rect.y == j:
                 m.is_obstacle = True
 
-    #Ostacolo 2
+    # Ostacolo 2
     j = GRIDSIZE * 10
     for i in range(400, window.get_width(), GRIDSIZE):
         for m in map:
@@ -75,9 +85,10 @@ def map_create():
     return surface, map
 
 
-def rotate(image, sprite, angle):
+def rotate(image, x, y, angle):
+    angle = angle * (180 / pi)
     rotated_image = pg.transform.rotozoom(image, angle, 1)
-    rotated_rect = rotated_image.get_rect(center=(sprite.x, sprite.y))
+    rotated_rect = rotated_image.get_rect(center=(x, y))
     return rotated_image, rotated_rect
 
 
@@ -87,14 +98,23 @@ def main():
     pg.display.set_icon(icon)
     is_running = True
     surface, map = map_create()
+    matrix = np.zeros([int(window.get_width() / GRIDSIZE), int(window.get_height() / GRIDSIZE)])
 
     # Robots
-    robot = Robot(100, window.get_height() - 100)
+    robot = Robot(110, window.get_height() - 110)
 
+    for m in map:
+        if math.dist(robot.rect.center, m.rect.center) < 1:
+            robot.reachable_matrix[int(m.rect.x / GRIDSIZE)][int(m.rect.y / GRIDSIZE)] = 1
     while is_running:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 is_running = False
+            elif event.type == pg.KEYDOWN:
+                seed()
+                rand = randint(0, len(standard_delta) - 1)
+                robot.delta = standard_delta[rand]
+
         for m in map:
             if not m.found:
                 if pg.sprite.collide_circle(robot, m):
@@ -102,17 +122,23 @@ def main():
                         m.color = BLACK
                     else:
                         m.color = WHITE
-                    m.found = True
+                        matrix[int(m.rect.x / GRIDSIZE)][int(m.rect.y / GRIDSIZE)] = 1
+                    m.found = 1
                     pg.draw.rect(surface, m.color, m.rect)
-        """
-        robot.delta += 30
-        robot.image, robot.rect = rotate(robot_image, robot, robot.delta)
-        """
+            if robot.rect.colliderect(m) and m.color == BLACK:
+                seed()
+                rand = randint(0, len(standard_delta) - 1)
+                robot.delta = standard_delta[rand]
+                print(standard_delta[rand]*(180/pi), robot.delta*(180/pi))
+
+        robot.update(matrix)
+        x, y = robot.rect.center
+        robot.image, robot.rect = rotate(robot_image, x, y, robot.delta)
         window.blit(surface, (0, 0))
         window.blit(robot.image, robot.rect)
-        pg.draw.rect(window, BLACK, robot.rect, 1)
-        robot.update()
         pg.display.update()
+
+        # time.sleep(10)
 
 
 if __name__ == '__main__':